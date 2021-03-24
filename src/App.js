7ae348d1ebff6ffef2b3449c25777a3c873673c9
TannenMoneyshot <tannenmoneyshot@icloud.com>
2021-03-24 14:38:06 +0100

    .env added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
     cartItemsList: []
   }
 
+  API_URL = process.env.REACT_APP_API_URL
+
   async componentDidMount() {
     await this.fetchItemsFromAPI()
 
@@ -19,8 +21,9 @@ class App extends Component {
 
   fetchItemsFromAPI = async () => {
 
-    const responseProducts = await fetch('http://localhost:8082/api/products')
-    const responseItems = await fetch('http://localhost:8082/api/items')
+    
+    const responseProducts = await fetch(`${this.API_URL}/api/products`)
+    const responseItems = await fetch(`${this.API_URL}/api/items`)
     const jsonProducts = await responseProducts.json()
     console.log(jsonProducts)
     const jsonItems = await responseItems.json()
@@ -46,7 +49,7 @@ class App extends Component {
     const potentialDuplicate = this.state.cartItemsList.find((item) => item.product_id === itemToAdd.product_id)
     if (potentialDuplicate !== undefined) {
       itemToAdd.quantity = parseInt(itemToAdd.quantity) + parseInt(potentialDuplicate.quantity)
-      await fetch(`http://localhost:8082/api/products/${potentialDuplicate.id}/items/${potentialDuplicate.id}`, {
+      await fetch(`${this.API_URL}/api/products/${potentialDuplicate.id}/items/${potentialDuplicate.id}`, {
         method: 'DELETE',
         body: JSON.stringify(potentialDuplicate),
         headers: {
@@ -55,7 +58,7 @@ class App extends Component {
         }
       })
     }
-    await fetch('http://localhost:8082/api/items', {
+    await fetch(`${this.API_URL}/api/items`, {
       method: 'POST',
       body: JSON.stringify(itemToAdd),
       headers: {
